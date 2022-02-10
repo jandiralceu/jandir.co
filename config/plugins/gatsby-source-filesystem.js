4b16b1611891e24fc6187adc2b8f994d6d09beb4
Jandir A. Cutabiala <jandir.dev@gmail.com>
2022-02-10 19:25:42 -0300

    chore: creating master pipeline

--- config/plugins/gatsby-source-filesystem.js
+++ config/plugins/gatsby-source-filesystem.js
@@ -8,5 +8,13 @@ module.exports = [
             path: `${path.resolve('./content/articles')}`,
             ignore: [`**/\.*`], // ignore files starting with a dot
         }
+    },
+    {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            name: `locale`,
+            path: `${path.resolve('./config/locales')}`,
+            ignore: [`**/\.*`], // ignore files starting with a dot
+        }
     }
 ]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from "react"
-import { Link } from "gatsby";
+import {Link, useTranslation} from 'gatsby-plugin-react-i18next';
 
 const IndexPage = () => {
+    const {t} = useTranslation();
+
   return (
     <main>
         <h1 className="text-3xl font-bold">Jandir Alceu</h1>
+        <h2>{t('Home')}</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dignissimos maxime, nemo nobis perferendis perspiciatis sapiente totam. Architecto culpa dicta doloribus expedita illo inventore possimus quos sed, ut velit vero?</p>
         <Link to="/blog">Blog</Link>
     </main>
