293955a3662471d4e2905e88bebf3c6d77e7a8b9
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2022-02-20 05:07:44 -0300

    fix: change email

--- src/components/ui/Subscribe/index.tsx
+++ src/components/ui/Subscribe/index.tsx
@@ -9,8 +9,8 @@ export const Subscribe = () => {
     },
     onSubmit: ({ name, email }) => {
       const msg = {
-        to: 'jandiralceu@gmail.com', // Change to your recipient
-        from: 'me@jandir.co', // Change to your verified sender
+        to: 'me@jandir.co', // Change to your recipient
+        from: 'jandiralceu@gmail.com', // Change to your verified sender
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>'
