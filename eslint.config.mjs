4c220f2f5d8971fe713f0c1d4e37812fef8e08f2
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-09-10 13:43:55 -0300

    chore: add lint validation inbto pre-push hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,7 @@ export default [
       ".cache/",
       ".gitignore",
       "yarn.lock",
+      ".nvimrc",
     ],
   },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
