b1fbd71304d01c6f97cb7a72fbdc233bf2a3b98b
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-09-12 00:56:20 -0300

    chore: configure cypress for e2e tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -22,6 +22,7 @@ const config = {
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
   setupFiles: [`<rootDir>/loadershim.js`],
+  testMatch: ["**/?(*.)+(test).[tj]s?(x)"],
 };
 
 export default config;
