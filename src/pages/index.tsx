5ea7405def1031cf1f5934a4a3030ac39be041e2
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-09-15 10:16:35 -0300

    feat: add new meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,11 +62,14 @@ export const Head: HeadFC = () => {
       <meta property="og:type" content="website" />
       <meta property="og:title" content={site.siteMetadata.title} />
       <meta property="og:description" content={site.siteMetadata.description} />
+      <meta property="og:url" content={site.siteMetadata.siteUrl} />
       <meta
         property="og:image"
         content={`${site.siteMetadata.siteUrl}${file.publicURL}`}
       />
-      <meta property="og:url" content={site.siteMetadata.siteUrl} />
+      <meta property="og:image:width" content="2116 " />
+      <meta property="og:image:height" content="2646" />
+      <meta property="og:image:type" content="image/jpeg" />
 
       {/* Twitter meta options */}
       <meta name="twitter:card" content="summary_large_image" />
