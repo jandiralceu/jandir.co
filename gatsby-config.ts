448c952f737cbe6501f88317fb27c744bb76b212
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-09-15 03:19:42 -0300

    chore: add purgecss to remove unused css

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -85,6 +85,12 @@ const config: GatsbyConfig = {
       },
     },
     {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+      },
+    },
+    {
       resolve: "gatsby-plugin-mdx",
       options: {
         extensions: [`.mdx`, `.md`],
