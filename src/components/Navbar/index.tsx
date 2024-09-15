81bf7d0d29ebf64323e9d00b1e26660264324e76
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-09-15 02:18:40 -0300

    refactor: put link inside an li on ul

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -37,19 +37,20 @@ export default function Navbar({
             : location.pathname === path;
 
         return (
-          <Link
-            key={path}
-            to={path}
-            className={[
-              "pb-2 ",
-              isCurrentRoute
-                ? `font-bold border-b-2 ${activeBorderClassName}`
-                : `border-b-0 hover:border-b-2 transition-all duration-300 ease-in ${hoverBorderClassName}`,
-            ].join(" ")}
-            title={name}
-          >
-            {name}
-          </Link>
+          <li key={path}>
+            <Link
+              to={path}
+              className={[
+                "pb-2 ",
+                isCurrentRoute
+                  ? `font-bold border-b-2 ${activeBorderClassName}`
+                  : `border-b-0 hover:border-b-2 transition-all duration-300 ease-in ${hoverBorderClassName}`,
+              ].join(" ")}
+              title={name}
+            >
+              {name}
+            </Link>
+          </li>
         );
       })}
       {children}
